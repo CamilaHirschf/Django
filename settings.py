111720ab2c41155695d8c2596486febabaf531bb
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2023-12-29 14:26:36 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Django.helloworld',
+    'helloworld.helloworld',
 ]
 
 MIDDLEWARE = [
